e1fda8b2a133840b12d4f268402831abfc3f1c42
Scott McKell <scott.g.mckell@gmail.com>
2018-05-26 18:42:15 -0600

    add testing and prepare for branch

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+	test "Loads static pages" do
+		get root_path
+		assert_response :success
+	end
+
 end
