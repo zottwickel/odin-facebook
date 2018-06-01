4b5d5ad65d3ff401cc1cde2d0c3dc368fb240ae9
Scott McKell <scott.g.mckell@gmail.com>
2018-06-01 16:47:47 -0600

    add views for friendships

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,5 +1,4 @@
 class FriendsController < ApplicationController
-	before_action :set_friends, except: [:index, :create]
 
 	def create
 		@friendship = Friendship.new(user_id: current_user.id, friend_id: params[:friend_id])
@@ -15,17 +14,10 @@ class FriendsController < ApplicationController
 	end
 
 	def index
-		@friendships = current_user.friend_requests.all
+		@friends = current_user.friendships.all
 	end
 
 	def destroy
-		@friendship.destroy
-		head :no_content
 	end
 
-	private
-
-	def set_friends
-		@friendship = Friendship.find(params[:id])
-	end
 end
