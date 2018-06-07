c4a6099a84ca6088c4a44bc3774dcc7ff26bd739
Scott McKell <scott.g.mckell@gmail.com>
2018-06-07 12:08:57 -0600

    production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,8 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  config.action_dispatch.default_headers.merge!('Cache-Control' => 'no-store, no-cache')
+
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
